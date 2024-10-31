5ebef40fdb6f6bb27e019c3469d5e3d82712d1de
Paphawadee287 <66070287@kmitl.ac.th>
2024-10-31 13:49:14 +0700

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,10 +1,41 @@
+
+#include <WiFiS3.h>
+#include <MQTTClient.h>
 #include <stdlib.h>
 #include <string.h>
+
+const char WIFI_SSID[] = "Ivory";          // CHANGE TO YOUR WIFI SSID
+const char WIFI_PASSWORD[] = "ploy2547";  // CHANGE TO YOUR WIFI PASSWORD
+
+const char MQTT_BROKER_ADRRESS[] = "mqtt-dashboard.com";  // CHANGE TO MQTT BROKER'S ADDRESS
+const int MQTT_PORT = 1883;
+const char MQTT_CLIENT_ID[] = "arduino-uno-r4-clientgdxytscux";  // CHANGE IT AS YOU DESIRE
+const char MQTT_USERNAME[] = "";                                            // CHANGE IT IF REQUIRED, empty if not required
+const char MQTT_PASSWORD[] = "";                                            // CHANGE IT IF REQUIRED, empty if not required
+
+// The MQTT topics that Arduino should publish/subscribe
+const char PUBLISH_TOPIC[] = "vansenger";    // CHANGE IT AS YOU DESIRE
+const char SUBSCRIBE_TOPIC[] = "vansenger";  // CHANGE IT AS YOU DESIRE
+
+const int PUBLISH_INTERVAL = 1 * 1000;  // 60 seconds
+
+WiFiClient network;
+MQTTClient mqtt = MQTTClient(256);
+
+unsigned long lastPublishTime = 0;
+
+const int trigPinleft = 1;
+const int echoPinleft = 0;
+const int trigPinright = 13;
+const int echoPinright = 10;
+
+long durationleft, durationright;
+int distanceleft, distanceright;
+int count=13;
 
 const int segments[] = {2, 3, 4, 5, 6, 7, 8}; // Segment pins a-g
 const int leftSegment = 11, rightSegment = 12;
 
-// Digit segment patterns (0-9)
 const int digitPatterns[][7] = {
   {1, 1, 1, 1, 1, 1, 0}, // 0
   {0, 1, 1, 0, 0, 0, 0}, // 1
@@ -18,24 +49,24 @@ const int digitPatterns[][7] = {
   {1, 1, 1, 1, 0, 1, 1}  // 9
 };
 
-const int trigPinleft = 1;
-const int echoPinleft = 0;
-const int trigPinright = 13;
-const int echoPinright = 10;
-
-long durationleft, durationright;
-int distanceleft, distanceright;
-int count=13;
-// const unsigned long timeThreshold = 1000; // กำหนดช่วงเวลาสูงสุดในการผ่านระหว่างเซ็นเซอร์ซ้ายและขวา (1 วินาที)
-
-// bool leftTriggered = false;
-// bool rightTriggered = false;
-// unsigned long leftTriggerTime = 0;
-// unsigned long rightTriggerTime = 0;
-
-
 void setup() {
-    Serial.begin(9600); // Initialize serial communication at 9600 bits per second
+  Serial.begin(9600);
+
+  int status = WL_IDLE_STATUS;
+  while (status != WL_CONNECTED) {
+    Serial.print("Arduino UNO R4 - Attempting to connect to SSID: ");
+    Serial.println(WIFI_SSID);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  // print your board's IP address:
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  connectToMQTT();
+
     pinMode(trigPinleft, OUTPUT);
     pinMode(echoPinleft, INPUT);
     pinMode(trigPinright, OUTPUT);
@@ -48,6 +79,45 @@ void setup() {
 }
 
 void loop() {
+  mqtt.loop();
+
+  if (millis() - lastPublishTime > PUBLISH_INTERVAL) {
+    sendToMQTT();
+    lastPublishTime = millis();
+  }
+}
+
+void connectToMQTT() {
+  // Connect to the MQTT broker
+  mqtt.begin(MQTT_BROKER_ADRRESS, MQTT_PORT, network);
+
+  // Create a handler for incoming messages
+  mqtt.onMessage(messageHandler);
+
+  Serial.print("Arduino UNO R4 - Connecting to MQTT broker");
+
+  while (!mqtt.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD)) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println();
+
+  if (!mqtt.connected()) {
+    Serial.println("Arduino UNO R4 - MQTT broker Timeout!");
+    return;
+  }
+
+  // Subscribe to a topic, the incoming messages are processed by messageHandler() function
+  if (mqtt.subscribe(SUBSCRIBE_TOPIC))
+    Serial.print("Arduino UNO R4 - Subscribed to the topic: ");
+  else
+    Serial.print("Arduino UNO R4 - Failed to subscribe to the topic: ");
+
+  Serial.println(SUBSCRIBE_TOPIC);
+  Serial.println("Arduino UNO R4 - MQTT broker Connected!");
+}
+
+void sendToMQTT() {
     // Clear the trigPin
     digitalWrite(trigPinleft, LOW);
     digitalWrite(trigPinright, LOW);
@@ -71,58 +141,10 @@ void loop() {
     distanceleft = durationleft * 0.034 / 2;
     distanceright = durationright * 0.034 / 2;
 
-    // Serial.print("distanceleft = ");
-    // Serial.println(distanceleft);
-    // Serial.print("distanceright = ");
-    // Serial.println(distanceright);
+    Serial.println(distanceleft);
+    Serial.println(distanceright);
 
-    // unsigned long currentTime = millis();
-
-    // // กรณีคนเริ่มผ่านเซ็นเซอร์ซ้ายก่อน
-    // if (distanceleft <= 50 && !leftTriggered) {
-    //     leftTriggerTime = currentTime;
-    //     leftTriggered = true;
-    //       // ตรวจจับว่าคนผ่านเซ็นเซอร์ขวาภายในเวลาที่กำหนดหลังจากซ้าย
-    //     if (distanceright <= 50 && leftTriggered && (currentTime - leftTriggerTime) <= timeThreshold) {
-    //         if (count > 0 && count <= 13) {
-    //             count--;
-    //             Serial.print("Count = ");
-    //             Serial.println(count);
-    //         } else {
-    //             Serial.println("FULL");
-    //         }
-    //         leftTriggered = false; // รีเซ็ตสถานะ
-    //     }
-    // }
-
-    // // กรณีคนเริ่มผ่านเซ็นเซอร์ขวาก่อน
-    // else if (distanceright <= 50 && !rightTriggered) {
-    //     rightTriggerTime = currentTime;
-    //     rightTriggered = true;
-    //         // ตรวจจับว่าคนผ่านเซ็นเซอร์ซ้ายภายในเวลาที่กำหนดหลังจากขวา
-    //     if (distanceleft <= 50 && rightTriggered && (currentTime - rightTriggerTime) <= timeThreshold) {
-    //         if (count >= 0 && count < 13) {
-    //             count++;
-    //             Serial.print("Count = ");
-    //             Serial.println(count);
-    //         } else {
-    //             Serial.println("Available");
-    //         }
-    //         rightTriggered = false; // รีเซ็ตสถานะ
-    //     }
-    // }
-
-    // // รีเซ็ตสถานะหากไม่ได้มีการผ่านอีกเซ็นเซอร์ภายในช่วงเวลาที่กำหนด
-    // if (leftTriggered && (currentTime - leftTriggerTime) > timeThreshold) {
-    //     leftTriggered = false;
-    // }
-    // if (rightTriggered && (currentTime - rightTriggerTime) > timeThreshold) {
-    //     rightTriggered = false;
-    // }
-
-    // delay(1000);
-
-    if (distanceleft <= 50) {
+  if (distanceleft <= 70) {
       if (count > 0 && count <= 13) {
         count--;
         Serial.print("count = ");
@@ -130,7 +152,7 @@ void loop() {
       }else {
         Serial.println("FULL");
       }
-  }else if (distanceright <= 50) {
+  }else if (distanceright <= 70) {
       if (count >= 0 && count < 13) {
         count++;
         Serial.print("count = ");
@@ -138,10 +160,7 @@ void loop() {
       }else {
         Serial.println("Available");
       }
-  }
-
   int digit1, digit2;
-  // for (int i = 0; i < ; i += 2) {
     if (count/10 == 0) {
     	digit1 = 0;
       digit2 = count%10;
@@ -149,42 +168,52 @@ void loop() {
     	digit1 = (int)count/10;
       digit2 = count%10;
     }
-    //int digit1 = text[i] - '0', digit2 = text[i + 1] - '0';
     showTwoDigits(digit1, digit2);
-  // }
     
-    delay(1000); // Wait 1 second before the next measurement
+    // delay(1000); // Wait 1 second before the next measurement
+  }
+
+
+  // int val = millis();
+  //int val = analogRead(A0);
+  String val_str = String(count);
+
+  // Convert the string to a char array for MQTT publishing
+  char messageBuffer[10];
+  val_str.toCharArray(messageBuffer, 10);
+
+  // Publish the message to the MQTT topic
+  mqtt.publish(PUBLISH_TOPIC, messageBuffer);
+
+  // Print debug information to the Serial Monitor in one line
+  Serial.println("Arduino UNO R4 - sent to MQTT: topic: " + String(PUBLISH_TOPIC) + " | payload: " + String(messageBuffer));
+}
+
+
+void messageHandler(String &topic, String &payload) {
+  Serial.println("Arduino UNO R4 - received from MQTT: topic: " + topic + " | payload: " + payload);
+
+  // You can process the incoming data , then control something
+  /*
+  process something
+  */
 }
 
 void showTwoDigits(int digit1, int digit2) {
   unsigned long startTime = millis();
-  while (millis() - startTime < 1000) {
-    // แสดง digit1 บน rightSegment
+  while (millis() - startTime < 100) {
     displayDigit(digit1, rightSegment);
-    delay(10);  // เพิ่มเวลาแสดงผล digit1 ให้ยาวขึ้น
-    clearSegments(); // ปิดการแสดงผลทั้งหมดชั่วคราว
-
-    // แสดง digit2 บน leftSegment
+    delay(2);
     displayDigit(digit2, leftSegment);
-    delay(10);  // เพิ่มเวลาแสดงผล digit2 ให้ยาวขึ้น
-    clearSegments(); // ปิดการแสดงผลทั้งหมดชั่วคราว
-  }
-}
-
-// ฟังก์ชันสำหรับการปิดทุก segment
-void clearSegments() {
-  digitalWrite(leftSegment, LOW);
-  digitalWrite(rightSegment, LOW);
-  for (int i = 0; i < 7; i++) {
-    digitalWrite(segments[i], LOW);
+    delay(2);
   }
 }
 
 void displayDigit(int digit, int activeSegment) {
-  digitalWrite(activeSegment, HIGH); // เปิดใช้งาน segment ที่ต้องการ
+  digitalWrite(leftSegment, activeSegment == leftSegment ? HIGH : LOW);
+  digitalWrite(rightSegment, activeSegment == rightSegment ? HIGH : LOW);
+
   for (int i = 0; i < 7; i++) {
     digitalWrite(segments[i], digitPatterns[digit][i] ? HIGH : LOW);
   }
 }
-
-
