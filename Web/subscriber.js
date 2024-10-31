48152502223226aede0266de686eb2df37d490c1
Ploy <116996366+mallowdoodee@users.noreply.github.com>
2024-10-31 11:20:38 +0700

    Connect web site mqtt with js

---
+++ Web/subscriber.js
@@ -0,0 +1,46 @@
+let mqttClient;
+
+window.addEventListener("load", (event) => {
+  connectToBroker();
+}
+
+function connectToBroker() {
+  const clientId = "client" + Math.random().toString(36).substring(7);
+
+  // Change this to point to your MQTT broker
+  const host = "ws://192.168.100.22:9001/mqtt";
+
+  const options = {
+    keepalive: 60,
+    clientId: clientId,
+    protocolId: "MQTT",
+    protocolVersion: 4,
+    clean: true,
+    reconnectPeriod: 1000,
+    connectTimeout: 30 * 1000,
+  };
+
+  mqttClient = mqtt.connect(host, options);
+
+  mqttClient.on("error", (err) => {
+    console.log("Error: ", err);
+    mqttClient.end();
+  });
+
+  mqttClient.on("reconnect", () => {
+    console.log("Reconnecting...");
+  });
+
+  mqttClient.on("connect", () => {
+    console.log("Client connected:" + clientId);
+  });
+
+  // Received
+  mqttClient.on("message", (topic, message, packet) => {
+    console.log(
+      "Received Message: " + message.toString() + "\nOn topic: " + topic
+    );
+    const messageTextArea = document.querySelector("#message");
+    messageTextArea.value += message + "\r\n";
+  });
+}
